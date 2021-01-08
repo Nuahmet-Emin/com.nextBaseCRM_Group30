95399360c12ac3f327d72862b9dcb56c4080cb67
Amannisa Sawuti <asawutiwupuer.15@gmail.com>
2021-01-08 14:37:12 -0500

    Added testNg dependency

--- src/test/java/com/cybertek/nextbaseCRM/Amannisa/CreatePoll.java
+++ src/test/java/com/cybertek/nextbaseCRM/Amannisa/CreatePoll.java
@@ -1,7 +1,23 @@
 package com.cybertek.nextbaseCRM.Amannisa;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class CreatePoll {
     public static void main(String[] args) {
+
+        //set up WebDriverManager
+        WebDriverManager.chromedriver().setup();
+        //open browser
+        WebDriver driver = new ChromeDriver();
+        //open web Browser
+        //Thread.sleep(3000);
+        driver.get("https://login2.nextbasecrm.com");
+        driver.manage().window().maximize();
+
+
+
 
     }
 }
