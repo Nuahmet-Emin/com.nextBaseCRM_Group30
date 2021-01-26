2a6c804d4812ff32988b041723c45f475bc5206c
Lyubov Ubushin <Lyubau1124@gmail.com>
2021-01-25 20:12:06 -0500

    Added MessageFunction1

--- src/test/java/com/cybertek/nextbaseCRM/Lyubov/MessageFunction.java
+++ src/test/java/com/cybertek/nextbaseCRM/Lyubov/MessageFunction.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class MessageFunction {
 
         public static void main(String[] args) throws InterruptedException {
+//Test1
 
             //set up WebDriverManager
             WebDriverManager.chromedriver().setup();
