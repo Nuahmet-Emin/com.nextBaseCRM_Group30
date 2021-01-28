9243cef680917017d422503be705b9401b933d65
petartanakov <petartanakov@gmail.com>
2021-01-28 11:41:25 -0500

    second test

--- src/test/java/com/cybertek/nextbaseCRM/PetarTanakov/Test2.java
+++ src/test/java/com/cybertek/nextbaseCRM/PetarTanakov/Test2.java
@@ -1,10 +1,9 @@
 package com.cybertek.nextbaseCRM.PetarTanakov;
 
-public class Test1 {
+public class Test2 {
 
     public static void main(String[] args) {
 
-        System.out.println(" Cybertek is the best ");
-
+        System.out.println(" Group 30 is the best ");
     }
 }
